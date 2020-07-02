b6c9f7a2da1ba84a8adf3f2076d79058127b7e26
pamAdmin <>
2020-07-02 17:30:41 -0500

    {/src/main/java/com/samplespace/service_add_upgrade/ServiceRequest.java}

--- src/main/java/com/samplespace/service_add_upgrade/ServiceRequest.java
+++ src/main/java/com/samplespace/service_add_upgrade/ServiceRequest.java
@@ -6,12 +6,81 @@ package com.samplespace.service_add_upgrade;
 
 public class ServiceRequest implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public ServiceRequest() {
-    }
+	@org.kie.api.definition.type.Label(value = "Customer Id")
+	private java.lang.String custId;
+	@org.kie.api.definition.type.Label(value = "Service Id")
+	private java.lang.String servId;
+	@org.kie.api.definition.type.Label(value = "Credit Approved")
+	private java.lang.Boolean creditApproved;
+	@org.kie.api.definition.type.Label(value = "Contractive Active")
+	private java.lang.Boolean contractActive;
+	@org.kie.api.definition.type.Label(value = "Account Balance ")
+	private java.lang.Integer accBal;
+	@org.kie.api.definition.type.Label(value = "Service Type")
+	private java.lang.String servType;
 
+	public ServiceRequest() {
+	}
 
+	public java.lang.String getCustId() {
+		return this.custId;
+	}
 
+	public void setCustId(java.lang.String custId) {
+		this.custId = custId;
+	}
+
+	public java.lang.String getServId() {
+		return this.servId;
+	}
+
+	public void setServId(java.lang.String servId) {
+		this.servId = servId;
+	}
+
+	public java.lang.Boolean getCreditApproved() {
+		return this.creditApproved;
+	}
+
+	public void setCreditApproved(java.lang.Boolean creditApproved) {
+		this.creditApproved = creditApproved;
+	}
+
+	public java.lang.Boolean getContractActive() {
+		return this.contractActive;
+	}
+
+	public void setContractActive(java.lang.Boolean contractActive) {
+		this.contractActive = contractActive;
+	}
+
+	public java.lang.Integer getAccBal() {
+		return this.accBal;
+	}
+
+	public void setAccBal(java.lang.Integer accBal) {
+		this.accBal = accBal;
+	}
+
+	public java.lang.String getServType() {
+		return this.servType;
+	}
+
+	public void setServType(java.lang.String servType) {
+		this.servType = servType;
+	}
+
+	public ServiceRequest(java.lang.String custId, java.lang.String servId,
+			java.lang.Boolean creditApproved, java.lang.Boolean contractActive,
+			java.lang.Integer accBal, java.lang.String servType) {
+		this.custId = custId;
+		this.servId = servId;
+		this.creditApproved = creditApproved;
+		this.contractActive = contractActive;
+		this.accBal = accBal;
+		this.servType = servType;
+	}
 
 }
